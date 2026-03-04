be1afe0c32ec2ad2720333439c345909e30e1f43
jocarbonell <joc5756@students.uc-bcf.edu.ph>
2026-03-05 00:09:17 +0800

    updated settings gear

--- title_page.gd
+++ title_page.gd
@@ -1,9 +1,13 @@
 extends Sprite2D
 
+@onready var settings_btn = $"../SettingsPanel/Settings"
+
 func _ready():
 	set_process_unhandled_input(true)
+	settings_btn.visible = false
 
 func _unhandled_input(event):
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
 			visible = false
+			settings_btn.visible = true
