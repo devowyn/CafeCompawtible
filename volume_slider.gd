214ad57f7aaa21944161ba6caabbfe6ca7046aa5
jools <jyo0584@students.uc-bcf.edu.ph>
2026-03-19 00:57:24 +0800

    fakyoo

--- volume_slider.gd
+++ volume_slider.gd
@@ -1,12 +1,15 @@
 extends HSlider
 
-func _ready():
-	custom_minimum_size = Vector2(0, 0)
-	min_value = -40.0
-	max_value = 0.0
-	value = 0.0
-	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Music"), 0.0)
-	value_changed.connect(_on_value_changed)
+# The '%' tells it to find the Unique Node, and '@onready' tells it to WAIT until the game is loaded!
+@onready var music_player = %MusicPlayer
 
-func _on_value_changed(new_value):
-	get_parent().get_parent().on_music_slider_changed(new_value)
+func _ready():
+	if music_player != null:
+		value = db_to_linear(music_player.volume_db)
+
+func _on_value_changed(new_value: float) -> void:
+	if music_player != null:
+		music_player.volume_db = linear_to_db(new_value)
+		
+		if new_value <= 0.0001:
+			music_player.volume_db = -80.0
--- volume_slider_sfx.gd
+++ volume_slider_sfx.gd
@@ -1,12 +1,18 @@
 extends HSlider
 
-func _ready():
-	custom_minimum_size = Vector2(0, 0)
-	min_value = -40.0
-	max_value = 0.0
-	value = 0.0
-	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("SFX"), 0.0)
-	value_changed.connect(_on_value_changed)
+# This creates a slot in the Inspector. Drag your MusicPlayer node into it!
+@export var music_player: AudioStreamPlayer
 
-func _on_value_changed(new_value):
-	get_parent().get_parent().on_sfx_slider_changed(new_value)
+func _ready():
+	# Sync the slider to whatever the volume currently is when the game starts
+	if music_player != null:
+		value = db_to_linear(music_player.volume_db)
+
+func _on_value_changed(new_value: float) -> void:
+	if music_player != null:
+		# Change the volume using Godot's built-in decibel translator
+		music_player.volume_db = linear_to_db(new_value)
+		
+		# Truly mute it if dragged all the way to the left
+		if new_value <= 0.0001:
+			music_player.volume_db = -80.0
